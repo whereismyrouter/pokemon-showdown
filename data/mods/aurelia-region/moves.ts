26eb955683d9ac639d0da7170b12093e5c6c6437
whereismyrouter <lazyspaghetti40@gmail.com>
2026-09-20 18:31:16 -0300

    Create moves.ts

---
+++ data/mods/aurelia-region/moves.ts
@@ -0,0 +1,16 @@
+	boilingslapdown: {
+		num: -2003, // Custom ID number
+		accuracy: 100,
+		basePower: 85,
+		category: "Physical",
+		name: "Boiling Slapdown",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1}, // Has the contact flag as requested
+		secondary: {
+			chance: 20, // 20% base chance to burn
+			status: 'brn',
+		},
+		target: "normal",
+		type: "Water",
+	},
