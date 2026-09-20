4d8ab21deb5cd8026ee87996dea0d15585780ccc
whereismyrouter <lazyspaghetti40@gmail.com>
2026-09-20 17:26:46 -0300

    Create pokedex.ts

---
+++ data/mods/aurelia-region/Pokedex.ts
@@ -0,0 +1,12 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+
+	solenogre: {
+		num: 10001,
+		name: "Solenogre",
+		types: ["Normal", "Electric"],
+		baseStats: {hp: 110, atk: 120, def: 100, spa: 5, spd: 100, spe: 36},
+		abilities: {0: "Iron Fist", 1: "Unaware", H: "Dizzy Stumble"},
+		weightkg: 150, // Le puse 150kg por su estilo de tanque pesón, puedes cambiarlo si quieres!
+	},
+
+};
