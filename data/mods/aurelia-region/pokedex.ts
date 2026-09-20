805d3c042bcd4092b4a99c58f7a10383a2bdd768
whereismyrouter <lazyspaghetti40@gmail.com>
2026-09-20 17:53:57 -0300

    Update pokedex.ts

--- data/mods/aurelia-region/pokedex.ts
+++ data/mods/aurelia-region/pokedex.ts
@@ -11,13 +11,11 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 
 };
 
-export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	rougeent: {
-		num: 10002, // The next unique index number
+		num: 10002,
 		name: "Rougeent",
 		types: ["Normal", "Fighting"],
-		baseStats: {hp: 61, atk: 90, def: 70, spa: 10, spd: 70, spe: 110}, // Total: 411
+		baseStats: {hp: 61, atk: 90, def: 70, spa: 10, spd: 70, spe: 110},
 		abilities: {0: "Guts", 1: "Merciless", H: "Double Whip"},
-		weightkg: 35.0, // Adjust weight as needed
+		weightkg: 35.0,
 	},
-};
