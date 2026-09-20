ef0c379eb2960ec0cfccbff5eec954037bd43c11
whereismyrouter <lazyspaghetti40@gmail.com>
2026-09-20 20:54:11 -0300

    Update conditions.ts

--- data/mods/aurelia-region/conditions.ts
+++ data/mods/aurelia-region/conditions.ts
@@ -19,3 +19,41 @@ export const Conditions: {[k: string]: ModdedConditionData} = {
 		},
 	},
 };
+
+	primalsnow: {
+		name: "Primal Snow",
+		effectType: "Weather",
+		duration: 0, // Lasts indefinitely as long as Brumaroth is active on the field
+		onStart(battle, source, effect) {
+			this.add('-weather', 'Primal Snow');
+			this.add('-message', "A cataclysmic, neverending winter has frozen the battlefield over!");
+		},
+		onResidualPriority: 1,
+		onResidual(battle) {
+			this.add('-weather', 'Primal Snow', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		// --- Constant Hail Chip Damage ---
+		onWeather(target) {
+			if (!target.hasType('Ice')) {
+				this.damage(target.baseMaxHP / 16);
+			}
+		},
+		// --- Boosts Ice-type attacks by 50% ---
+		onBasePowerPriority: 1,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.type === 'Ice') {
+				return this.chainModify(1.5);
+			}
+		},
+		// --- Prevents and destroys Aurora Veil ---
+		onSideConditionStart(sideCondition, side, source) {
+			if (sideCondition.id === 'auroraveil') {
+				this.add('-message', "The sheer pressure of the Primal Snow overcharged and shattered the Aurora Veil!");
+				return false; // Blocks Aurora Veil from being created entirely
+			}
+		},
+		onEnd() {
+			this.add('-weather', 'none');
+		},
+	},
