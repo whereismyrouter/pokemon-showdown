3eebd572fbea91bff36635963763429b82b4a783
whereismyrouter <lazyspaghetti40@gmail.com>
2026-09-20 17:33:43 -0300

    Create abilities.ts

---
+++ data/mods/aurelia-region/abilities.ts
@@ -0,0 +1,26 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+
+	dizzystumble: {
+		name: "Dizzy Stumble",
+		shortDesc: "When this Pokemon's HP is lower than 40%, foes become confused.",
+		onAfterMoveSecondarySelf(pokemon, attack, target) {
+			if (!pokemon.m.dizzyStumbleTriggered && pokemon.hp <= pokemon.maxhp * 0.40) {
+				pokemon.m.dizzyStumbleTriggered = true; 
+				this.add('-ability', pokemon, 'Dizzy Stumble');
+				for (const target Foe of pokemon.adjacentFoes()) {
+					targetFoe.addVolatile('confusion');
+				}
+			}
+		},
+		onDamage(damage, target, source, effect) {
+			if (!target.m.dizzyStumbleTriggered && target.hp <= target.maxhp * 0.40) {
+				target.m.dizzyStumbleTriggered = true;
+				this.add('-ability', target, 'Dizzy Stumble');
+				for (const target Foe of target.adjacentFoes()) {
+					targetFoe.addVolatile('confusion');
+				}
+			}
+		},
+	},
+
+};
