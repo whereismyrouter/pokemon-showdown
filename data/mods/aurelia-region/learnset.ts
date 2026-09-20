87e5a861f1a00bd8dc6d8de0cfb398d0a09e1616
whereismyrouter <lazyspaghetti40@gmail.com>
2026-09-20 18:06:57 -0300

    Create learnset.ts

---
+++ data/mods/aurelia-region/learnset.ts
@@ -0,0 +1,83 @@
+	solenogre: {
+		learnset: {
+			// --- Your Specific Move Requests ---
+			playrough: ["9M"],
+			wildcharge: ["9M"],
+			bulkup: ["9M"],
+			facade: ["9M"],
+			quickattack: ["9L1"],
+			extremespeed: ["9M"],
+			fissure: ["9M"],
+			headsmash: ["9M"],
+			bodypress: ["9M"],
+			gigaimax: ["9M"], // Internal ID for Giga Impact is 'gigaimpact'
+			gigaimpact: ["9M"],
+			stompingtantrum: ["9M"],
+			zenheadbutt: ["9M"],
+			suckerpunch: ["9L1"],
+			foulplay: ["9M"],
+			darkestlariat: ["9M"],
+			seismictoss: ["9M"],
+			throatchop: ["9M"],
+			bind: ["9L1"],
+        
+			// --- Every Single Fist/Punching Move ---
+			bulletpunch: ["9M"],
+			cometpunch: ["9L1"],
+			counter: ["9M"],
+			drainingpunch: ["9M"],
+			dynamicpunch: ["9M"],
+			firepunch: ["9M"],
+			focuspunch: ["9M"],
+			icepunch: ["9M"],
+			machpunch: ["9M"],
+			megapunch: ["9M"],
+			meteorash: ["9M"], // Internal ID for Meteor Mash is 'meteormash'
+			meteormash: ["9M"],
+			plasmafists: ["9M"],
+			poweruppunch: ["9M"],
+			shadowpunch: ["9M"],
+			skyuppercut: ["9M"],
+			thunderpunch: ["9M"],
+			wickedblow: ["9M"], // Fits perfectly as a blunt Dark-type punch!
+
+			// --- Additional Fighting-Type Moves ---
+			closecombat: ["9M"],
+			drainpunch: ["9M"], // Handled under drainingpunch / drainpunch depending on version, adding both for safety
+			superpower: ["9M"],
+			lowkick: ["9M"],
+			reversal: ["9M"],
+			brickbreak: ["9M"],
+		},
+	},
+
+        
+  	rougeent: {
+		learnset: {
+			// Level-up moves
+			vinewhip: ["9L1"],
+			tailwhip: ["9L1"],
+			pound: ["9L1"],
+			quickattack: ["9L4"],
+			doublekick: ["9L9"],
+			shadowclaw: ["9L15"],
+			bulkup: ["9L20"],
+			powerwhip: ["9L28"],
+			firewhip: ["9L35"],
+			brickbreak: ["9L42"],
+			closecombat: ["9L50"],
+
+			// TM / Machine moves
+			agility: ["9M"],
+			phantomforce: ["9M"],
+			superfang: ["9M"],
+			ironhead: ["9M"],
+			direclaw: ["9M"],
+			facade: ["9M"],
+			protect: ["9M"],
+			substitute: ["9M"],
+			swordsdance: ["9M"],
+			uturn: ["9M"],
+		},
+	},
+        
