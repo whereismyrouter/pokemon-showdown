a0b8d4fbbe5c4a841f87379d4099150822c835d5
whereismyrouter <lazyspaghetti40@gmail.com>
2026-09-21 01:29:48 -0300

    Update items.ts

--- data/mods/aurelia-region/items.ts
+++ data/mods/aurelia-region/items.ts
@@ -13,3 +13,26 @@ export const Items: {[k: string]: ModdedItemData} = {
 		shortDesc: "Mega Evolves Golurk into Mega Golurk-Z during battle.",
 	},
 };
+
+	nidokingite: {
+		name: "Nidokingite",
+		spritenum: 575,
+		megaStone: "Nidoking-Mega",
+		megaEvolves: "Nidoking",
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.name === 'Nidoking') || pokemon.baseSpecies.name === 'Nidoking') return false;
+			return true;
+		},
+		shortDesc: "Mega Evolves Nidoking into Mega Nidoking during battle.",
+	},
+	nidoqueenite: {
+		name: "Nidoqueenite",
+		spritenum: 575,
+		megaStone: "Nidoqueen-Mega",
+		megaEvolves: "Nidoqueen",
+		onTakeItem(item, pokemon, source) {
+			if ((source && source.baseSpecies.name === 'Nidoqueen') || pokemon.baseSpecies.name === 'Nidoqueen') return false;
+			return true;
+		},
+		shortDesc: "Mega Evolves Nidoqueen into Mega Nidoqueen during battle.",
+	},
